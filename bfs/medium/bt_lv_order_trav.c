994450e222fc9c282fef22abfb1c51de0e693f16
Rohan Hangal <rohanhangal@gmail.com>
2025-03-04 22:01:03 -0800

    binary tree lv order trav complete

--- bfs/medium/bt_lv_order_trav.c
+++ bfs/medium/bt_lv_order_trav.c
@@ -5,9 +5,9 @@
 
 /*
  * Solution:
- *
+ * First we handle the edge case of root being NULL and set the returnSize and returnColumnSizes accordingly
+ * Next we need to initialize our resolution as an array of arrays int** with a size of 2000 based on max constraints given
  */
-
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -22,5 +22,34 @@
  * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
  */
 int** levelOrder(struct TreeNode* root, int* returnSize, int** returnColumnSizes) {
-    
+    if (!root) {
+        *returnSize = 0;
+        *returnColumnSizes = NULL;
+        return NULL;
+    }
+
+    int** res = (int**)malloc(2000 * sizeof(int*));
+    *returnColumnSizes = (int*)malloc(2000 * sizeof(int));
+    *returnSize = 0;
+
+    struct TreeNode* queue[2000];  // Single queue storing nodes directly
+    int front = 0, rear = 0;
+    queue[rear++] = root;
+
+    while (front < rear) {
+        int levelSize = rear - front;
+        res[*returnSize] = (int*)malloc(levelSize * sizeof(int));
+        (*returnColumnSizes)[*returnSize] = levelSize;
+
+        for (int i = 0; i < levelSize; i++) {
+            struct TreeNode* node = queue[front++];
+            res[*returnSize][i] = node->val;
+
+            if (node->left) queue[rear++] = node->left;
+            if (node->right) queue[rear++] = node->right;
+        }
+        (*returnSize)++;
+    }
+
+    return res;
 }
