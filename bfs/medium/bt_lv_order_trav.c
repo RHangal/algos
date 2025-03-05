830c913401b21f9e0f8603dae7e1cf1d4ca5f807
Rohan Hangal <rohanhangal@gmail.com>
2025-03-05 00:22:12 -0800

    solution written

--- bfs/medium/bt_lv_order_trav.c
+++ bfs/medium/bt_lv_order_trav.c
@@ -7,6 +7,18 @@
  * Solution:
  * First we handle the edge case of root being NULL and set the returnSize and returnColumnSizes accordingly
  * Next we need to initialize our resolution as an array of arrays int** with a size of 2000 based on max constraints given
+ * Initialize returnColumnSizes as 2000 times size of int as its an array holding up to 2000 ints
+ * Initialize returnSize as 0
+ * Initialize que which is an array of struct TreeNode* up to 2000 
+ * Set root as que[0] and increment rear
+ * We move from left to right (front to rear) in a while loop front<rear
+ * Figure out level size by subtracting rear-front
+ * allocate memory for the res at the returnSize
+ * set return column size
+ * Loop through from 0 < levelsize and use a temp node to store whats in the que while incrementing the front
+ * append the value of the temp node to our res array
+ * Check left and right node and append to the queue if they exist (left to right order)
+ * Increment return size after each loop
  */
 /**
  * Definition for a binary tree node.
